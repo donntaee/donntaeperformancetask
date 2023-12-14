6de7857a6747b6e248c2ef905b06f636d6b0a93b
donntaee <121972426+donntaee@users.noreply.github.com>
2023-12-14 14:31:37 -0800

    Player scene didn't get competed

--- Player.gd
+++ Player.gd
@@ -9,4 +9,16 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("move right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move left"):
+		velocity.x -= -1
+	if Input.is_action_pressed("move up"):
+		velocity.y += 1
+	if Input.is_action_pressed("move down"):
+		velocity.y += 1
+	
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed 
+	position += velocity * delta
